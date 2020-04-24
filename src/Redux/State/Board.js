5549617620c04eadaaef2f307b98b94a13d8b952
Joris Langlois <joris.langlois@knplabs.com>
2020-04-24 12:47:03 +0200

    final layout

--- src/Redux/State/Board.js
+++ src/Redux/State/Board.js
@@ -12,80 +12,178 @@ export const MAIN_CHARACTER = {
 const INITIAL_STATE = {
   meh: false,
   lines: [
+    // l6
+    [
+      {
+        x: 0,
+        y: 5,
+        char: null,
+        locked: false,
+      }, {
+        x: 1,
+        y: 5 ,
+        char: null,
+        locked: false,
+      }, {
+        x: 2,
+        y: 5,
+        char: null,
+        locked: true,
+      }, {
+        x: 3,
+        y: 5 ,
+        char: null,
+        locked: false,
+      }, {
+        x: 4,
+        y: 5,
+        char: null,
+        locked: false,
+      }
+    ],
+    // l5
+    [
+      {
+        x: 0,
+        y: 4,
+        char: null,
+        locked: false,
+      }, {
+        x: 1,
+        y: 4 ,
+        char: null,
+        locked: false,
+      }, {
+        x: 2,
+        y: 4,
+        char: null,
+        locked: false,
+      }, {
+        x: 3,
+        y: 4 ,
+        char: null,
+        locked: false,
+      }, {
+        x: 4,
+        y: 4,
+        char: null,
+        locked: false,
+      }
+    ],
     // l4
     [
       {
-          x: 0,
-          y: 3,
-          char: null,
-          locked: false,
+        x: 0,
+        y: 3,
+        char: null,
+        locked: false,
       }, {
-          x: 1,
-          y: 3 ,
-          char: null,
-          locked: false,
+        x: 1,
+        y: 3 ,
+        char: null,
+        locked: false,
       }, {
-          x: 2,
-          y: 3,
-          char: null,
-          locked: false,
+        x: 2,
+        y: 3,
+        char: null,
+        locked: false,
+      }, {
+        x: 3,
+        y: 3 ,
+        char: null,
+        locked: false,
+      }, {
+        x: 4,
+        y: 3,
+        char: null,
+        locked: false,
       }
     ],
     // l3
     [
         {
-            x: 0,
-            y: 2,
-            char: null,
-            locked: false,
+          x: 0,
+          y: 2,
+          char: null,
+          locked: true,
         }, {
-            x: 1,
-            y: 2 ,
-            char: null,
-            locked: true,
+          x: 1,
+          y: 2 ,
+          char: null,
+          locked: false,
         }, {
-            x: 2,
-            y: 2,
-            char: null,
-            locked: false,
+          x: 2,
+          y: 2,
+          char: MAIN_CHARACTER,
+          locked: false,
+        }, {
+          x: 3,
+          y: 2 ,
+          char: null,
+          locked: false,
+        }, {
+          x: 4,
+          y: 2,
+          char: null,
+          locked: true,
         }
     ],
     // l2
     [
         {
-            x: 0,
-            y: 1,
-            char: MAIN_CHARACTER,
-            locked: false,
+          x: 0,
+          y: 1,
+          char: null,
+          locked: true,
         }, {
-            x: 1,
-            y: 1,
-            char: null,
-            locked: false,
+          x: 1,
+          y: 1,
+          char: null,
+          locked: false,
         }, {
-            x: 2,
-            y: 1,
-            char: null,
-            locked: false,
+          x: 2,
+          y: 1,
+          char: null,
+          locked: false,
+        }, {
+          x: 3,
+          y: 1 ,
+          char: null,
+          locked: false,
+        }, {
+          x: 4,
+          y: 1,
+          char: null,
+          locked: true,
         }
     ],
     // l1
     [
         {
-            x: 0,
-            y: 0,
-            char: null,
-            locked: true,
+          x: 0,
+          y: 0,
+          char: null,
+          locked: true,
         }, {
-            x: 1,
-            y: 0,
-            char: null,
-            locked: false,
+          x: 1,
+          y: 0,
+          char: null,
+          locked: true,
         }, {
-            x: 2,
-            y: 0,
-            char: null,
-            locked: false,
+          x: 2,
+          y: 0,
+          char: null,
+          locked: false,
+        }, {
+          x: 3,
+          y: 0,
+          char: null,
+          locked: true,
+        }, {
+          x: 4,
+          y: 0,
+          char: null,
+          locked: true,
         }
     ],
   ]
