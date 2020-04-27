a7492d472d8f5c29edc2bb510ed0ed0d705c4293
Joris Langlois <joris.langlois@knplabs.com>
2020-04-27 13:29:49 +0200

    introductive text

--- src/Component/Game/Game.js
+++ src/Component/Game/Game.js
@@ -13,6 +13,19 @@ export default ({
       <Board />
     </section>
     <section className="game-section right-panel">
+      <h1>La chasse aux poussins est ouverte !</h1>
+
+      <p className="explanations">
+        Utilises les flèches de ton clavier pour déplacer le
+        &nbsp;
+        <img src="/images/chick_face.png" alt="chick"/> et emmener les
+        &nbsp;
+        <img src="/images/fox_face.png" alt="fox" /> dans leur terrier
+        &nbsp;
+        <img src="/images/burrow.png" alt="burrow" />.
+      </p>
+      <p>Mais prend garde à ne pas te faire manger !</p>
+
       <button className="retry" onClick={retry}>
         Recommencer !
       </button>
