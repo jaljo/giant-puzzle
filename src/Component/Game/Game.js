e01bef17d82e5877b51472d0e57db975165a72e8
Joris Langlois <joris.langlois@knplabs.com>
2020-04-28 12:37:41 +0200

    final texts

--- src/Component/Game/Game.js
+++ src/Component/Game/Game.js
@@ -13,24 +13,42 @@ export default ({
       <Board />
     </section>
     <section className="game-section right-panel">
-      <h1>La chasse aux poussins est ouverte !</h1>
+      {(!gameWon && !gameOver) &&
+        <>
+          <h1>La chasse aux poussins est ouverte !</h1>
+          <p>
+            Bravo, tu es arrivé jusqu'ici ! Je suis l'<b>agent CC</b> et j'ai une
+            mauvaise nouvelle pour toi, tu t'es transformé en poussin&nbsp;
+            <img src="/images/chick_face.png" alt="chick"/> !
+          </p>
+          <p>
+            Il va falloir survivre en faisant tomber les renards&nbsp;
+            <img src="/images/fox_face.png" alt="fox" /> dans leurs terriers&nbsp;
+            <img src="/images/burrow.png" alt="burrow" /> grâce aux flèches de
+            ton clavier.
+          </p>
+          <p>Mais prend garde à ne pas te faire manger !</p>
+        </>
+      }
 
-      <p className="explanations">
-        Utilises les flèches de ton clavier pour déplacer le
-        &nbsp;
-        <img src="/images/chick_face.png" alt="chick"/> et emmener les
-        &nbsp;
-        <img src="/images/fox_face.png" alt="fox" /> dans leur terrier
-        &nbsp;
-        <img src="/images/burrow.png" alt="burrow" />.
-      </p>
-      <p>Mais prend garde à ne pas te faire manger !</p>
+      {gameWon &&
+        <>
+          <p className="emphasis">Félicitations !</p>
+          <p>
+            Tu as réussi à sauver ta peau ! Tu es le futur&nbsp;
+            <b><i>Sherlock Holmes</i></b> de notre équipe !
+          </p>
+        </>
+      }
+
+      {gameOver &&
+        <p className="emphasis">
+          GAME OVER !
+        </p>
+      }
 
       <button className="retry" onClick={retry}>
         Recommencer !
       </button>
-
-      {gameOver && <p className="game-over">GAME OVER</p>}
-      {gameWon && <p className="game-won">GG !</p>}
     </section>
   </div>
